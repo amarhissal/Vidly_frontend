163e9e2710846d042bf03546943e24782c16c30e
Amar <amarshissal224@gmail.com>
2023-12-23 14:39:41 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Route,Redirect,Switch} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Vidly from './components/vidly';
+import Rentals from './components/common/rentals';
+import loginServices from './components/services/loginServices';
+import Customer from './components/common/customer';
+import NotFound from './components/common/notfound';
+import Navbar from './components/navbarcounter';
+import MovieForm from './components/common/movieform'
+import LoginForm from './components/common/loginform';
+import RegisterForm from './components/common/registerforms';
+import Logout from './components/common/logout';
+import './App.css'
+import 'react-toastify/dist/ReactToastify.css'
+import ProtectedRoute from './components/common/protectedrout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {  } 
+
+componentDidMount() {
+ const user = loginServices.getCurrentUSer()
+ this.setState({user})
+
 }
 
+  render() { 
+    const {user} = this.state
+    return (
+      <>
+      <ToastContainer/>
+    <Navbar  user={this.state.user}/>
+    <main className='container'>
+<Switch>
+    <Route path='/register' component={RegisterForm}></Route>
+    <Route path='/login' component={LoginForm}></Route>
+    <Route path='/logout' component={Logout}></Route>
+    <ProtectedRoute path='/movies/:id' component={MovieForm}  />
+    <Route path='/movies'
+    render={props=><Vidly {...props} user={this.state.user}/>} ></Route>
+    <Route path='/customers' component={Customer}></Route>
+    <Route path='/rentals' component={Rentals}></Route>
+    <Route path='/notfound' component={NotFound}></Route>
+    <Redirect from='/' exact to='/movies'></Redirect> 
+    <Redirect to='/notfound'></Redirect>
+    </Switch>
+    </main>
+    </>
+    );
+  }
+}
+ 
 export default App;
